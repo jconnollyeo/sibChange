0e29684000ef2d2a52e653a422d1c25e853e6463
Jacob Connolly <py15jmc@hex.leeds.ac.uk>
2023-03-23 11:45:16 +0000

    Adapted to run on time series of images

--- Sibling_change_detection.py
+++ Sibling_change_detection.py
@@ -24,11 +24,6 @@ from datetime import datetime
 wdir = "/nfs/a1/homes/py15jmc/"
 ifgs = np.load(wdir + "complex.npy")
 
-ix1, ix2 = 100, 101
-
-ifg1 = ifgs[ix1]
-ifg2 = ifgs[ix2]
-
 SHP_i = np.load(wdir + "bootstrap/shp_i_20150511_20170617.npy")
 SHP_j = np.load(wdir + "bootstrap/shp_j_20150511_20170617.npy")
 
@@ -36,44 +31,73 @@ n_siblings = np.sum(~np.isnan(SHP_i) * ~np.isnan(SHP_j), axis=0)
 
 coherence_2015 = np.load(wdir + "bootstrap/coherence_2015.npy")
 
-metrics = SF.generateMetricsIFG(ifg1, ifg2, SHP_i, SHP_j)
+# ix1, ix2 = 100, 101
 
-df = pd.DataFrame(metrics, columns = np.array(["i", "j", "n_siblings", "jk_std", "jk_bias", "amp_mean", "amp_std", "amp_px", "poi_diff", "max_amp_diff", "actual_coherence", "apparent_coherence"]))
-# df = pd.read_csv("/nfs/a1/homes/py15jmc/bootstrap/2023/IFG_100_101_20230214_162302.csv")
+for ix1, ix2 in zip(np.arange(115, 160), np.arange(116, 161)):
+    if ix1 in np.arange(80, 111):
+        pass
+    else:
+        ifg1 = ifgs[ix1]
+        ifg2 = ifgs[ix2]
 
-dropped_mask = np.ones(ifg1.shape, dtype=bool)
+        metrics = SF.generateMetricsIFG(ifg1, ifg2, SHP_i, SHP_j)
 
-# Remvoe any nan values
-for ix in list(set(np.where(np.isnan(df))[0])):
-    
-    dropped_mask[int(df["i"][ix]), int(df["j"][ix])] = False
-    df = df.drop(ix)
+        df = pd.DataFrame(metrics, columns = np.array(["i", "j", "n_siblings", "jk_std", "jk_bias", "amp_mean", "amp_std", "amp_px", "poi_diff", "max_amp_diff", "actual_coherence", "apparent_coherence"]))
+        # df = pd.read_csv("/nfs/a1/homes/py15jmc/bootstrap/2023/IFG_100_101_20230214_162302.csv")
 
-# Remove any pixels that have fewer than 15 siblings. 
-for ix in list(set(np.where(df["n_siblings"] < 15)[0])):
-    
-    dropped_mask[int(df["i"][ix]), int(df["j"][ix])] = False
-    df = df.drop(ix)
+        dropped_mask = np.ones(ifg1.shape, dtype=bool)
 
-df.to_csv(f"IFG_{ix1}_{ix2}_{datetime.strftime(datetime.now(), '%Y%m%d_%H%M%S')}.csv")
-RF = joblib.load("RF_20230215_121116.jbl")
+        # Remove any nan values
+        ix_nan = list(set(np.where(np.isnan(df))[0]))
+        for ix in ix_nan:
+            # print (f"Dropping bc nan: {ix}")
+            try:
+                dropped_mask[int(df["i"][ix]), int(df["j"][ix])] = False
+            except:
+                print (f"Error : {ix}")
 
-predictions = RF.predict(df[["jk_std", "jk_bias", "amp_mean", "max_amp_diff", "poi_diff", "apparent_coherence"]])
+        # Remove any pixels that have fewer than 15 siblings. 
+        ix_sibs = list(set(np.where(df["n_siblings"] < 15)[0]))
+        for ix in ix_sibs:
+            # print (f"Dropping bc sibs: {ix}")
+            try: 
+                dropped_mask[int(df["i"][ix]), int(df["j"][ix])] = False
+            except:
+                print (f"Error : {ix}")
+        
+        for ix in list(set(ix_nan + ix_sibs)):
+            print (f"Dropping {ix}")
+            df = df.drop(ix)
 
-predictions_arr = np.zeros(ifg1.shape, dtype=bool)
+        np.save(f"timeseries/dropped_mask_{ix1}_{ix2}_{datetime.strftime(datetime.now(), '%Y%m%d_%H%M%S')}.npy", dropped_mask)
 
-predictions_arr[dropped_mask] = predictions
+        df.to_csv(f"timeseries/IFG_{ix1}_{ix2}_{datetime.strftime(datetime.now(), '%Y%m%d_%H%M%S')}.csv")
+        RF = joblib.load("RF_20230215_121116.jbl")
 
-np.save("predictions.npy", predictions_arr)
-# pred_arr = predictions.reshape(ifg1.shape)
+        features = df[["jk_std", "jk_bias", "amp_mean", "max_amp_diff", "poi_diff", "apparent_coherence"]]
+        predictions = RF.predict(features)
 
-fig, ax = plt.subplots()
-ax.matshow(np.mean(abs(ifgs), axis=0), cmap="binary_r")
-ax.matshow(SF.falseNaN(predictions_arr), cmap="RdBu")
+        predictions_arr = np.zeros(ifg1.shape, dtype=bool)
 
-fig, ax = plt.subplots()
-mask_siblings, predictions_filt = SF.filterPredictions(predictions_arr, SHP_i, SHP_j, 5)
-ax.matshow(np.mean(abs(ifgs), axis=0), cmap="binary_r")
-ax.matshow(SF.falseNaN(predictions_filt), cmap="RdBu")
+        predictions_arr[dropped_mask] = predictions
+        
+        probs_arr = np.zeros(ifg1.shape, dtype=float)
 
-plt.show()
+        probs = RF.predict_proba(features)[:, 0]
+        
+        probs_arr[dropped_mask] = probs
+        np.save(f"timeseries/probabilities_{ix1}_{ix2}_{datetime.strftime(datetime.now(), '%Y%m%d_%H%M%S')}.npy", probs_arr)
+        np.save(f"timeseries/predictions_{ix1}_{ix2}_{datetime.strftime(datetime.now(), '%Y%m%d_%H%M%S')}.npy", predictions_arr)
+
+        # pred_arr = predictions.reshape(ifg1.shape)
+
+        # fig, ax = plt.subplots()
+        # ax.matshow(np.mean(abs(ifgs), axis=0), cmap="binary_r")
+        # ax.matshow(SF.falseNaN(predictions_arr), cmap="RdBu")
+
+        # fig, ax = plt.subplots()
+        # mask_siblings, predictions_filt = SF.filterPredictions(predictions_arr, SHP_i, SHP_j, 5)
+        # ax.matshow(np.mean(abs(ifgs), axis=0), cmap="binary_r")
+        # ax.matshow(SF.falseNaN(predictions_filt), cmap="RdBu")
+
+        # plt.show()
